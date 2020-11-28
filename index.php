14187cd428fc5169736071a46320f4b76a554273
Malusi Pakade <pakadem@gmail.com>
2020-11-28 16:11:24 +0200

    added landing page, getAll, read

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+    <style type="text/css">
+        .wrapper{
+            width: 1024px;
+            margin: 0 auto;
+        }
+        .page-header h2{
+            margin-top: 0;
+        }
+        table tr td:last-child a{
+            margin-right: 15px;
+        }
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();   
+        });
+    </script>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header clearfix">
+                        <h2 class="">Employees Time Management System</h2>
+                    </div>
+                    <div class="page-header clearfix">
+                        <a href="all.php" class="btn btn-success ">View Employees</a>
+                    </div>
+
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+
+<?php
+    // echo "<pre>";
+    // print_r( $item_array );
+?>
+</html>
