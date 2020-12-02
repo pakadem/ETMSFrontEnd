78b52cd6fba294901c8a5de438cde03fdc21a305
shake <shakes150e@gmail.com>
2020-12-02 21:01:25 +0200

    Store is updated

--- store/update.php
+++ store/update.php
@@ -18,7 +18,7 @@ $item_array = json_decode($item_json, true);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Create Employee</title>
+    <title>Create Store</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         .wrapper{
